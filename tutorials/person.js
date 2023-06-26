c530ba68e821d0dfdfd3698fc20e7ce87ee0f1b8
Sophie Nguyen <sophiengocnguyen113@gmail.com>
2023-06-25 20:24:25 -0400

    Initial Commit

---
+++ tutorials/person.js
@@ -0,0 +1,25 @@
+// Module Wrapper Function
+// (function (exports, require, module, __filename, __dirname) {
+
+// })
+
+// console.log(__dirname, __filename);
+
+// const personn = {
+//     name: 'Sophie Nguyen',
+//     age: 20
+// }
+
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    greeting() {
+      console.log(`My name is ${this.name} and I am ${this.age}`);
+    }
+  }
+
+module.exports = Person;
+    
